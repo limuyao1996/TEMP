1136313568287aa84cc4a5efc1caa04accc4ca72
Li Muyao <911407614@qq.com>
2019-12-28 06:23:20 +0800

    fix(配置): 修改配置文件
    怎么换行呢？？？？？

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,41 @@
-module.exports = {extends: ['@commitlint/config-angular']};
+module.exports = {
+    extends: ['@commitlint/config-angular'],
+
+    /**
+     *  git commit提交格式必须为 
+     *              <type>[optional scope]: <subject>
+     *              [optional body]
+     *              [optional footer]
+     *
+     * 例如：feat(模块名称):新增xxx特性 （简短描述）
+     *      新增xxx、xxx、xxx功能 （简单的详细描述）
+     * 
+     * 配置规则说明见：https://commitlint.js.org/#/reference-rules
+     */
+    rules: {
+        'type-enum': [
+            2,
+            'always',
+            [
+                'WIP',      // 开发中
+                'feat',     // 新特性
+                'improvement', // 加强现有特性
+                'fix',      // 修补bug
+                'refactor', // 重构
+                'docs',     // 文档
+                'test',     // 单元测试
+                'config',   // 配置文件
+                'style',    // 格式需改
+                'perf',     // 性能提升
+                'ci',       // ci
+                'revert',   // 版本回退
+                'chore',    // 其他修改
+            ],
+        ],
+        'type-case': [2, 'always', 'lowerCase'],  // type必须消息
+        'type-empty': [2, 'never'],               // type不能为空
+        'scope-empty': [2, 'never'],              // scope不能为空
+        'subject-empty': [2, 'never'],            // subject（简短得描述）不能为空
+        'subject-max-length': [1, 'always', 50],  // subject最大长度，超出只会警告，不阻止提交
+      }
+};
